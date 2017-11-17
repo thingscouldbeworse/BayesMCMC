c87a9ebbea4c81fa38783b8470e073eb121ccf1d
Kirk Hardy <megal33tdude@gmail.com>
2017-11-17 10:08:22 -0500

    cleaned up less for readability and more for fun

--- run.py
+++ run.py
@@ -1,40 +1,20 @@
 import random
 
-def check_a(b, d):
-    checker = random.uniform(0,1)
-    if(b and d):
-        return checker <= 0.8873239437
-    if(not b and d):
-        return checker <= 0.2727272727
-    if(not b and not d):
-        return checker <= 0.8571428571
-    if(b and not d):
-        return checker <= 0.9921259843
-
-def check_b(a):
-    checker = random.uniform(0,1)
-    if(a):
-        return checker <= 0.7
-    if not a:
-        return checker <= 0.1
-
-def check_d(a, e):
-    checker = random.uniform(0,1)
-    if(a and e):
-        return checker <= 0.02702
-    if(not a and e):
-        return checker <= 0.30769
-    if(a and not e):
-        return checker <= 0.6923
-    if(not a and not e):
-        return checker <= 0.973
-
-def check_e(d):
-    checker = random.uniform(0,1)
-    if d:
-        return checker <= 0.1
-    if not d:
-        return checker <= 0.9
+#        abde
+probabilities = {
+        'x11x': 0.8873239437,
+        'x01x': 0.2727272727,
+        'x00x': 0.8571428571,
+        'x10x': 0.9921259843,
+        '1xxx': 0.7,
+        '0xxx': 0.1,
+        'xx1x': 0.1,
+        'xx0x': 0.9,
+        '1xx1': 0.02702,
+        '0xx1': 0.30769,
+        '1xx0': 0.6923,
+        '0xx0': 0.973
+        }
 
 counter = 0
 iterations = 10000
@@ -46,12 +26,16 @@ e = random.randint(0,1)
 print(a, b, d, e)
 
 for i in range(0,iterations):
-
-    a = (1 if check_a(b, d) else 0)
-    b = (1 if check_b(a) else 0)
-    d = (1 if check_d(a, e) else 0)
-    e = (1 if check_e(d) else 0)
     
+    checker = random.uniform(0,1)
+    a = (1 if checker <= probabilities['x'+str(b)+str(d)+'x'] else 0) 
+    checker = random.uniform(0,1)
+    b = (1 if checker <= probabilities[str(a)+'xxx'] else 0) 
+    checker = random.uniform(0,1)
+    d = (1 if checker <= probabilities[str(a)+'xx'+str(e)]else 0)
+    checker = random.uniform(0,1)
+    e = (1 if checker <= probabilities['xx'+str(d)+'x'] else 0)
+
     if i % 100 == 0 and i > 1:
         print(counter/i)
     if b:
